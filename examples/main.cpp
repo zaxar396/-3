031919ed66f4d842a85a835cb578b6dfa6da687e
NastaseaBorz <Nastaseaborzenkova@mail.ru>
2016-09-22 21:32:56 +0300

    Add files via upload

---
+++ examples/main.cpp
@@ -0,0 +1,5 @@
+#include <print_hello_world.h>
+
+int main(){
+    print_hello_world();
+}
---
+++ sources/print_hello_world.cpp
@@ -0,0 +1,1 @@
+#include <iostream>#include <print_hello_world.h>using namespace std;void print_hello_world(){    cout << "Hello world" << endl;}
